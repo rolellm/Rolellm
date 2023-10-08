e7ebb3048cc9d8f79e407ab9c17ba9bae7442ddf
zhangzhigang <15735658567@163.com>
2023-10-08 16:57:20 +0800

    第二次提交

---
+++ src/components/company.vue
@@ -0,0 +1,36 @@
+<template>
+<span class="nameT">
+  <span class="valT">
+    {{num}}
+  </span>
+  {{val}}
+  ;
+</span>
+</template>
+
+<script>
+export default {
+  name: "name",
+  props:{
+    val: String,
+    num: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.valT{
+  position: relative;
+  top: -4px;
+  left: 3px;
+  /*font-weight: 500;*/
+  /*font-size: 1rem;*/
+}
+.nameT{
+  font-weight: 500;
+  font-size: 0.9rem;
+}
+</style>
---
+++ src/components/name.vue
@@ -0,0 +1,36 @@
+<template>
+<span class="nameT">
+  {{val}}
+  <span class="valT">
+    {{num}}
+  </span>
+  ,
+</span>
+</template>
+
+<script>
+export default {
+  name: "name",
+  props:{
+    val: String,
+    num: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.valT{
+  position: relative;
+  top: -4px;
+  left: 1px;
+  font-weight: 500;
+  font-size: 1rem;
+}
+.nameT{
+  font-weight: 700;
+  font-size: 1.1rem;
+}
+</style>
